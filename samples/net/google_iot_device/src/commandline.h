3b3babb760d2f7c44f84957bf8a1180069dd34db
Arpad Tigyi <atigyi@google.com>
2019-06-27 13:35:00 +0200

    First version of a new sample app: samples/net/google_iot_device. This sample uses the Google offered embedded c device sdk: https://github.com/GoogleCloudPlatform/iot-device-sdk-embedded-c through a 'west' module in modules/lib/google/iot-device-sdk-embedded-c.

---
+++ samples/net/google_iot_device/src/commandline.h
@@ -0,0 +1,18 @@
+/*
+ * This module implements a command line argument parser.
+ */
+
+#include <iotc_mqtt.h>
+
+/* Flags set by commandline arguments. */
+extern iotc_mqtt_qos_t iotc_example_qos;
+
+/* Parameters returned by the parser. These will be in a structure someday. */
+extern const char* iotc_project_id;
+extern const char* iotc_device_path;
+extern const char* iotc_publish_topic;
+extern const char* iotc_publish_message;
+extern const char* iotc_private_key_filename;
+
+int iotc_parse(int argc, char** argv, char* valid_options,
+               const unsigned options_length);
