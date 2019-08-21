d57a50cdf3697fbaf5364b9fa6566ecb8d7d36a6
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2019-08-21 14:58:19 +0300

    samples: google_iot_device: Remove "command line" and replace with config.c
    There is no "command line" in Zephyr RTOS. "Command line" which was
    used previously is an artifact of a special-purpose, testing target
    "native_posix", not portable to any other Zephyr target.
    
    So, instead provide config.c file, where a user can define all needed
    application parameters (which previously were passed on "command
    line"). As such a file contain credentials, it should be never put
    under version control. This commit includes a template file,
    config.c.in, which can be copied by a user to config.c, and required
    values filled in.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

---
+++ samples/net/google_iot_device/src/config.h
@@ -0,0 +1,18 @@
+/* Copyright 2019 Google LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * Declarations for application config options.
+ */
+
+#include <iotc_mqtt.h>
+
+extern iotc_mqtt_qos_t iotc_example_qos;
+
+extern const char* iotc_project_id;
+extern const char* iotc_device_path;
+extern const char* iotc_private_key_pem;
+extern const char* iotc_publish_topic;
+extern const char* iotc_publish_message;
--- samples/net/google_iot_device/src/example_utils.c
+++ samples/net/google_iot_device/src/example_utils.c
@@ -7,7 +7,7 @@
 #include <iotc_jwt.h>
 #include <stdio.h>
 
-#include "commandline.h"
+#include "config.h"
 #include "example_utils.h"
 
 #define IOTC_UNUSED(x) (void)(x)
@@ -16,90 +16,6 @@ extern iotc_crypto_key_data_t iotc_connect_private_key_data;
 
 static iotc_timed_task_handle_t delayed_publish_task =
     IOTC_INVALID_TIMED_TASK_HANDLE;
-
-int iotc_example_handle_command_line_args(int argc, char* argv[]) {
-  char options[] = "h:p:d:t:m:f:";
-  int missingparameter = 0;
-  int retval = 0;
-
-  /* log the executable name and library version */
-  printf("\n%s\n%s\n", argv[0], iotc_cilent_version_str);
-
-  /* Parse the argv array for ONLY the options specified in the options string
-   */
-  retval = iotc_parse(argc, argv, options, sizeof(options));
-
-  if (-1 == retval) {
-    /* iotc_parse has returned an error, and has already logged the error
-       to the console. Therefore just silently exit here. */
-    return -1;
-  }
-
-  /* Check to see that the required parameters were all present on the command
-   * line */
-  if (NULL == iotc_project_id) {
-    missingparameter = 1;
-    printf("-p --project_id is required\n");
-  }
-
-  if (NULL == iotc_device_path) {
-    missingparameter = 1;
-    printf("-d --device_path is required\n");
-  }
-
-  if (NULL == iotc_publish_topic) {
-    missingparameter = 1;
-    printf("-t --publish_topic is required\n");
-  }
-
-  if (1 == missingparameter) {
-    /* Error has already been logged, above.  Silently exit here */
-    printf("\n");
-    return -1;
-  }
-
-  return 0;
-}
-
-int load_ec_private_key_pem_from_posix_fs(char* buf_ec_private_key_pem,
-                                          size_t buf_len) {
-  FILE* fp = fopen(iotc_private_key_filename, "rb");
-  if (fp == NULL) {
-    printf("ERROR!\n");
-    printf(
-        "\tMissing Private Key required for JWT signing.\n"
-        "\tPlease copy and paste your device's EC private key into\n"
-        "\ta file with the following path based on this executable's\n"
-        "\tcurrent working dir:\n\t\t\'%s\'\n\n"
-        "\tAlternatively use the --help command line parameter to learn\n"
-        "\thow to set a path to your file using command line arguments\n",
-        iotc_private_key_filename);
-    return -1;
-  }
-
-  fseek(fp, 0, SEEK_END);
-  long file_size = ftell(fp);
-  rewind(fp);
-
-  if ((size_t)file_size > buf_len) {
-    printf(
-        "private key file size of %lu bytes is larger that certificate buffer "
-        "size of %lu bytes\n",
-        file_size, (long)buf_len);
-    fclose(fp);
-    return -1;
-  }
-
-  long bytes_read = fread(buf_ec_private_key_pem, 1, file_size, fp);
-  fclose(fp);
-
-  if (bytes_read != file_size) {
-    printf("could not fully read private key file\n");
-    return -1;
-  }
-
-  return 0;
-}
 
 void on_connection_state_changed(iotc_context_handle_t in_context_handle,
                                  void* data, iotc_state_t state) {
